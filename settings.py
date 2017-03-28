bf60a1f5cbbd75ab401d91634305859dc6509bd6
olivere <oe@bink.com>
2017-03-28 11:07:18 +0100

    Update auth_key for spreedly.

--- settings.py
+++ settings.py
@@ -8,15 +8,9 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = env_var('CYCLOPS_DEBUG', False)
 
-# user name
-KEY = env_var('CYCLOPS_KEY', 'Yc7xn3gDP73PPOQLEB2BYpv31EV')
-
-# password
-ACCESS_SECRET = env_var('CYCLOPS_ACCESS_SECRET', '4m8tVKWHvakjhfBXnRVDbfkOArB9NvAyo9U6lWtVulb2thuI6T439blRbQWGwQcH')
-
 # This AUTH_KEY is the interpreted key based on the ACCESS_SECRET and KEY above
-AUTH_KEY = env_var('CYCLOPS_AUTH_KEY', 'WWM3eG4zZ0RQNzNQUE9RTEVCMkJZcHYzMUVWOjRtOHRWS1dIdmFramhmQlhuUlZEYmZrT0FyQj'
-                                       'lOdkF5bzlVNmxXdFZ1bGIydGh1STZUNDM5YmxSYlFXR3dRY0g=')
+AUTH_KEY = env_var('CYCLOPS_AUTH_KEY', 'MUxmN0RpS2drY3g1QW53N1F4V2REeGFLdFRhOjRtOHRWS1dIdmFramhmQlhuUlZEYm'
+                                       'ZrT0FyQjlOdkF5bzlVNmxXdFZ1bGIydGh1STZUNDM5YmxSYlFXR3dRY0g=')
 
 EMAIL_SOURCE_CONFIG = [
     # user | password | host | port
