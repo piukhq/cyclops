ec2d3b3ad81822aa83d3b4b0bd12dbfe6775c5c9
olivere <oe@bink.com>
2017-04-11 12:18:37 +0100

    Major overhaul of cyclops scheduling functionality.

--- settings.py
+++ settings.py
@@ -1,12 +1,13 @@
 from environment import env_var, read_env
 import os
+from raven import Client
 
 
 read_env()
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = env_var('CYCLOPS_DEBUG', False)
+DEBUG = env_var('CYCLOPS_DEBUG', True)
 
 # This AUTH_KEY is the interpreted key based on the ACCESS_SECRET and KEY above
 AUTH_KEY = env_var('CYCLOPS_AUTH_KEY', 'MUxmN0RpS2drY3g1QW53N1F4V2REeGFLdFRhOjRtOHRWS1dIdmFramhmQlhuUlZEYm'
@@ -21,3 +22,9 @@ EMAIL_SOURCE_CONFIG = [
 EMAIL_TARGETS = env_var('EMAIL_TARGET', ['oe@bink.com', 'dg@bink.com', 'ml@bink.com', 'pb@bink.com', ], )
 
 SLACK_API_TOKEN = 'xoxb-119487439522-Lsefc6ykOx3RIXC89WN8wx3h'
+
+SENTRY_DSN = env_var("CYCLOPS_SENTRY_DSN")
+
+sentry = Client(SENTRY_DSN)
+
+BREACH_CHECK_PERIOD = 10
