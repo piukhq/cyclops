10ebe95226b198e21827ab1bd533c7e254a0c55b
Chris Pressland <cpressland@bink.com>
2020-10-21 13:31:08 +0000

    Move Sentry to use Azure KeyVault

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-To create an AUTH_KEY, go into Postman and under Authorization set:
- Username to KEY and password to ACCESS_SECRET.  This will be 
- automatically translated into an AUTH_KEY as follows:
- "Basic abcde" for the Headers.  In the code we only use the AUTH_KEY 
- in Cyclops. 
+# Cyclops
+
+Redacts Spreedly Payment Gateways like a champ!
--- settings.py
+++ settings.py
@@ -1,31 +1,44 @@
+import base64
+import json
+from os import getenv, environ
+
 import sentry_sdk
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
-from environment import env_var, read_env
+SPREEDLY_AUTH = None
+MAILGUN_API_KEY = None
+MAILGUN_API = None
 
-read_env()
+if "KEYVAULT_URI" in environ:
+    KEYVAULT_URI = getenv("KEYVAULT_URI")
+    kvcredential = DefaultAzureCredential()
+    kvclient = SecretClient(vault_url=KEYVAULT_URI, credential=kvcredential)
+
+    SPREEDLY_USER = json.loads(kvclient.get_secret("spreedly-oAuthUsername").value)["value"]
+    SPREEDLY_PASS = json.loads(kvclient.get_secret("spreedly-oAuthPassword").value)["value"]
+    SPREEDLY_AUTH = base64.b64encode(f"{SPREEDLY_USER}:{SPREEDLY_PASS}".encode()).decode()
+
+    MAILGUN = json.loads(kvclient.get_secret("mailgun").value)
+    MAILGUN_API_KEY = MAILGUN["MAILGUN_API_KEY"]
+    MAILGUN_API = f"{MAILGUN['MAILGUN_API']}/{MAILGUN['MAILGUN_DOMAIN']}/messages"
+
+
+MAILGUN_SENDER = getenv("MAILGUN_SENDER", "itapps@bink.com")
+MAILGUN_TARGET = getenv("MAILGUN_TARGET", "cyclops@bink.com")
 
 SPREEDLY_BASE_URL = "https://core.spreedly.com"
 
-AUTH_KEY = env_var("CYCLOPS_AUTH_KEY")
-
-MAILGUN_API_KEY = env_var("MAILGUN_API_KEY", "key-63iepgmkm8qdzs0fxm05jy0oq3c1yd42")
-MAILGUN_SENDER = env_var("MAILGUN_SENDER", "support@uk.bink.com")
-MAILGUN_API = env_var("MAILGUN_URL", "https://api.mailgun.net/v3/uk.bink.com/messages")
-MAILGUN_TARGET = env_var("MAILGUN_TARGET", "cyclops@bink.com")
-
-TEAMS_WEBHOOK = env_var(
+TEAMS_WEBHOOK = getenv(
     "TEAMS_WEBHOOK",
     "https://outlook.office.com/webhook/bf220ac8-d509-474f-a568-148982784d19@a6e2367a-92ea-4e5a-b565-723830bcc095/IncomingWebhook/5931605317444706be22208c6f394f4c/48aca6b1-4d56-4a15-bc92-8aa9d97300df",  # noqa: E501
 )
 
-XMATTERS_WEBHOOK = env_var(
+XMATTERS_WEBHOOK = getenv(
     "XMATTERS_WEBHOOK",
     "https://bink.xmatters.com/api/integration/1/functions/83fd7b90-28c5-4323-8ebb-a6010c976634/triggers?apiKey=78c63de7-1ab8-4cf0-80d3-745755fe30b2",  # noqa: E501
 )
 
-SENTRY_DSN = env_var("SENTRY_DSN")
-SENTRY_ENV = env_var("SENTRY_ENV")
-if SENTRY_DSN:
-    sentry_sdk.init(dsn=SENTRY_DSN, environment=SENTRY_ENV)
+sentry_sdk.init()
 
-HEALTHCHECK_URL = env_var("HEALTHCHECK_URL", "https://hchk.io/92429b19-1ea5-4f45-8abf-ee93c943ac39")
+HEALTHCHECK_URL = getenv("HEALTHCHECK_URL", "https://hchk.io/92429b19-1ea5-4f45-8abf-ee93c943ac39")
